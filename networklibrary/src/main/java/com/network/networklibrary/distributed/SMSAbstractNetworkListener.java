fda7c7cf317746df55a29c07bb5477779ce88ebf
Marco Mariotto <mari8.marco@gmail.com>
2019-12-07 15:47:20 +0100

    Added a few classes
    Added listener, manager. I copied a few things from the replicated dictionary. Implementation of each method is the next step.

---
+++ networklibrary/src/main/java/com/network/networklibrary/distributed/SMSAbstractNetworkListener.java
@@ -0,0 +1,23 @@
+package com.network.networklibrary.distributed;
+
+import com.eis.smslibrary.SMSMessage;
+import com.eis.smslibrary.listeners.SMSReceivedServiceListener;
+
+
+public abstract class SMSAbstractNetworkListener extends SMSReceivedServiceListener
+{
+    @Override
+    public void onMessageReceived(SMSMessage message){
+                /*TODO check if incoming request is valid: if it is a join req, call onJoinProposal(),
+                  TODO otherwise call processRequest() of SMSAbstractNetworkManager
+                */
+    }
+
+    /**
+     * This method is for handling a JoinProposal. It is up to the application to override it.
+     *
+     * @param message as SMSMessage
+     */
+    public abstract void onJoinProposal(SMSMessage message);
+
+}
