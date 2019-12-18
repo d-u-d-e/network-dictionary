40cd648d90be78f2ed1e284554e46ab9cc48ea25
Alessandra Tonin <ale.tonin.98@gmail.com>
2019-12-18 15:47:58 +0100

    Added method refresh()

--- networklibrary/src/main/java/com/eis/networklibrary/kademlia/SMSDistributedNetworkDictionary.java
+++ networklibrary/src/main/java/com/eis/networklibrary/kademlia/SMSDistributedNetworkDictionary.java
@@ -19,7 +19,7 @@ public class SMSDistributedNetworkDictionary<RV> implements NetworkDictionary<SM
 
 
     //Maximum users per bucket
-    static final int KADEMLIA_K = 5; //TODO maximum size for SMS-based networks is k = 12 (otherwise a NODE_FOUND reply would contain more than 160 chars)
+    static final int KADEMLIA_K = 5; //TODO maximum size for SMS-based networks is K = 12 (otherwise a NODE_FOUND reply would contain more than 160 chars)
     // Number Of Buckets. We have a bucket for each bit
     static final int NO_BUCKETS = KADAddress.BIT_LENGTH;
     SMSKADPeer mySelf; //address of current node holding this dictionary
@@ -238,12 +238,10 @@ public class SMSDistributedNetworkDictionary<RV> implements NetworkDictionary<SM
     }
 
     /**
-     *
      * @param bucketIndex identifies each bucket, from 0 to N-1, where N = NO_BUCKETS.
      * @return a random KADAddress in this bucket
      */
-    public KADAddress getRandomAddressInBucket(int bucketIndex){
+    public KADAddress getRandomAddressInBucket(int bucketIndex) {
         return null;
     }
-
 }
