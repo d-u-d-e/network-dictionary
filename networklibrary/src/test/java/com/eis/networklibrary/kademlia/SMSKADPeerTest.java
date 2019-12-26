f6d54c6bb9d64e4281053698fff594c0340917f9
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2019-12-26 03:19:31 +0100

    Fixed tests

--- networklibrary/src/test/java/com/eis/networklibrary/kademlia/SMSKADPeerTest.java
+++ networklibrary/src/test/java/com/eis/networklibrary/kademlia/SMSKADPeerTest.java
@@ -3,30 +3,33 @@ package com.eis.networklibrary.kademlia;
 import com.eis.smslibrary.SMSPeer;
 import com.eis.smslibrary.exceptions.InvalidTelephoneNumberException;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author ALberto Ursino
  */
 public class SMSKADPeerTest {
 
-    final String PHONE_NUMBER = "+391111111111";
-    final String PHONE_NUMBER2 = "+391111111112";
-    final String WRONG_PHONE_NUMBER = "ciao";
-    final String ASSERT_FAIL_EXCEPTION_MESSAGE = "Should have thrown an exception";
-    final int COMPARE_NEG_INT = -1;
-    final int COMPARE_POS_INT = 1;
-    final int COMPARE_ZERO_INT = 0;
+    private final String PHONE_NUMBER = "+391111111111";
+    private final String PHONE_NUMBER2 = "+391111111112";
+    private String WRONG_PHONE_NUMBER = "ciao";
+    private String EXCEPTION_EXPECTED_MESSAGE = "Should have thrown an exception";
+    private int COMPARE_NEG_INT = -1;
+    private int COMPARE_POS_INT = 1;
+    private int COMPARE_ZERO_INT = 0;
 
-    SMSPeer smsPeer = new SMSPeer(PHONE_NUMBER);
-    KADAddress kadAddress = new KADAddress(new byte[]{106, 97, 118, 97, 32, 105, 115, 32, 111, 107});
-    KADAddress kadAddress2 = new KADAddress(new byte[]{107, 97, 118, 97, 32, 105, 115, 32, 111, 107});
-    KADAddress target = new KADAddress(new byte[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+    private SMSPeer smsPeer = new SMSPeer(PHONE_NUMBER);
+    private KADAddress kadAddress = new KADAddress(new byte[]{106, 97, 118, 97, 32, 105, 115, 32, 111, 107});
+    private KADAddress kadAddress2 = new KADAddress(new byte[]{107, 97, 118, 97, 32, 105, 115, 32, 111, 107});
+    private KADAddress target = new KADAddress(new byte[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
 
-    SMSKADPeer.SMSKADComparator kadComparator;
-    SMSKADPeer smsPeerKadPeer, stringKadPeer, string2KadPeer;
+    private SMSKADPeer.SMSKADComparator kadComparator;
+    private SMSKADPeer smsPeerKadPeer, stringKadPeer, string2KadPeer;
 
     @Before
     public void init() {
@@ -39,25 +42,25 @@ public class SMSKADPeerTest {
     @Test
     public void getNetworkAddress_kadAddresses_test() {
         smsPeerKadPeer.networkAddress = kadAddress;
-        Assert.assertEquals(kadAddress, smsPeerKadPeer.getNetworkAddress());
-        Assert.assertNotEquals(kadAddress2, smsPeerKadPeer.getNetworkAddress());
+        assertEquals(kadAddress, smsPeerKadPeer.getNetworkAddress());
+        assertNotEquals(kadAddress2, smsPeerKadPeer.getNetworkAddress());
     }
 
     @Test
     public void compare_smsKadPeer_test() {
         //Equals
-        Assert.assertEquals(COMPARE_ZERO_INT, kadComparator.compare(stringKadPeer, stringKadPeer));
+        assertEquals(COMPARE_ZERO_INT, kadComparator.compare(stringKadPeer, stringKadPeer));
         //First is closer
-        Assert.assertEquals(COMPARE_NEG_INT, kadComparator.compare(stringKadPeer, string2KadPeer));
+        assertEquals(COMPARE_NEG_INT, kadComparator.compare(stringKadPeer, string2KadPeer));
         //Second is closer
-        Assert.assertEquals(COMPARE_POS_INT, kadComparator.compare(string2KadPeer, stringKadPeer));
+        assertEquals(COMPARE_POS_INT, kadComparator.compare(string2KadPeer, stringKadPeer));
     }
 
-    @Test
+    @Test(expected = InvalidTelephoneNumberException.class)
     public void constructor_phoneNumber_invalid() {
         try {
             new SMSKADPeer(WRONG_PHONE_NUMBER);
-            Assert.fail(ASSERT_FAIL_EXCEPTION_MESSAGE);
+            fail(EXCEPTION_EXPECTED_MESSAGE);
         } catch (InvalidTelephoneNumberException e) {
             //Success
         }
