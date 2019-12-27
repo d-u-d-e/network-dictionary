2166fb9a4d6e12c347ebc42fbd8b42d4937187a3
Alessandra Tonin <ale.tonin.98@gmail.com>
2019-12-27 18:01:21 +0100

    Update ClosestPQTest

--- networklibrary/src/test/java/com/eis/networklibrary/kademlia/ClosestPQTest.java
+++ networklibrary/src/test/java/com/eis/networklibrary/kademlia/ClosestPQTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class ClosestPQTest {
 
@@ -13,6 +15,31 @@ public class ClosestPQTest {
     private static final SMSKADPeer.SMSKADComparator DEFAULT_COMPARATOR = new SMSKADPeer.SMSKADComparator(TARGET);
     private final ArrayList<SMSKADPeer> knownNodes = new ArrayList<>();
     private static final ArrayList<MutablePair<SMSKADPeer, Boolean>> DEFAULT_PAIRS = new ArrayList<>();
+
+    /**
+     * Compares the elements of two arrays as a set (doesn't care about position)
+     *
+     * @param array1
+     * @param array2
+     * @return
+     */
+    private boolean compareArray(Object[] array1, Object[] array2) {
+        HashSet<Object> set1 = new HashSet<>(Arrays.asList(array1));
+        HashSet<Object> set2 = new HashSet<>(Arrays.asList(array2));
+        return set1.equals(set2);
+    }
+
+    private boolean compareArray(Object[] array1, ArrayList array2) {
+        HashSet<Object> set1 = new HashSet<>(Arrays.asList(array1));
+        HashSet set2 = new HashSet<>(array2);
+        return set1.equals(set2);
+    }
+
+    private boolean compareArray(ArrayList array1, ArrayList array2) {
+        HashSet set1 = new HashSet<>(array1);
+        HashSet set2 = new HashSet<>(array2);
+        return set1.equals(set2);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -24,13 +51,17 @@ public class ClosestPQTest {
         knownNodes.add(new SMSKADPeer("+393589685772"));
         knownNodes.add(new SMSKADPeer("+393500685222"));
         knownNodes.add(new SMSKADPeer("+393589897522"));
+        closestPQ.add(new SMSKADPeer("+393489685222"), false);
+        closestPQ.add(new SMSKADPeer("+393589685772"), false);
+        closestPQ.add(new SMSKADPeer("+393500685222"), false);
+        closestPQ.add(new SMSKADPeer("+393589897522"), false);
     }
 
     @Test
     public void getTest() {
         SMSKADPeer peer = new SMSKADPeer("+393457090735");
         closestPQ.add(peer, false);
-        Assert.assertEquals(peer, knownNodes.get(0));
+        Assert.assertEquals(peer, closestPQ.get(0).first);
     }
 
     @Test
@@ -39,5 +70,7 @@ public class ClosestPQTest {
 
     @Test
     public void getAllPeersTest() {
+        fillKnownNodes();
+        Assert.assertTrue(compareArray(closestPQ.getAllPeers(), knownNodes));
     }
 }
